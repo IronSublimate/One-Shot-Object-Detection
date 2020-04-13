a5a2be2c126ba0c753166cc08305685c834ac4a6
IronSublimate <houyuxuan3120487@163.com>
2020-04-13 21:29:43 +0800

    cpu无法运行

--- lib/roi_data_layer/roibatchLoader.py
+++ lib/roi_data_layer/roibatchLoader.py
@@ -67,16 +67,17 @@ class roibatchLoader(data.Dataset):
 
                 self.ratio_list_batch[left_idx:(right_idx + 1)] = target_ratio
 
-        self._cat_ids = [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13,
-            14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-            24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
-            37, 38, 39, 40, 41, 42, 43, 44, 46, 47,
-            48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-            58, 59, 60, 61, 62, 63, 64, 65, 67, 70,
-            72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
-            82, 84, 85, 86, 87, 88, 89, 90
-        ]
+        # self._cat_ids = [
+        #     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13,
+        #     14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+        #     24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
+        #     37, 38, 39, 40, 41, 42, 43, 44, 46, 47,
+        #     48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+        #     58, 59, 60, 61, 62, 63, 64, 65, 67, 70,
+        #     72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
+        #     82, 84, 85, 86, 87, 88, 89, 90
+        # ]
+        self._cat_ids = list(range(1, 91))
         self._classes = {
             ind + 1: cat_id for ind, cat_id in enumerate(self._cat_ids)
         }
@@ -97,8 +98,8 @@ class roibatchLoader(data.Dataset):
 
         blobs = get_minibatch(minibatch_db, self._num_classes)
 
-        #blobs['gt_boxes'] = [x for x in blobs['gt_boxes'] if x[-1] in self.list_ind]
-        blobs['gt_boxes'] = np.array(blobs['gt_boxes']).reshape((-1,5))
+        blobs['gt_boxes'] = [x for x in blobs['gt_boxes'] if x[-1] in self.list_ind]
+        blobs['gt_boxes'] = np.array(blobs['gt_boxes']).reshape((-1, 5))
 
         if self.training:
             # Random choice query catgory
