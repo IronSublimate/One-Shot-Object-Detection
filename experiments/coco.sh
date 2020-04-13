5c49e1a1df2f0712f2e1ca7330cbd8948ad32280
IronSublimate <houyuxuan3120487@163.com>
2020-04-13 17:29:09 +0800

    错误无法解决

--- experiments/coco.sh
+++ experiments/coco.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=4,5 python trainval_net.py \
+CUDA_VISIBLE_DEVICES=2 python trainval_net.py \
     --dataset coco --net res50 \
     --bs 4 \
     --cuda --g 2 --seen 3 --mGPUs
